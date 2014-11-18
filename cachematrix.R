b6cfce2d871911f1aea4c79be626ad6e0fdc9b96
Kelly Richards <kellyrichards@Kellys-MacBook-Pro.local>
2014-11-17 22:51:49 -0800

    inital crack at prog assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The two functions below work together to precompute an inverse of a matrix efficiently through the 
+## use of a caching proxy
 
-## Write a short comment describing this function
-
+## creates a getter and setter to a matrix which allow a proxy to provide additional functionality.  
+## In thie case the extra functionality is storing a matrix 'next' to the matrix defined in the argument
+## list.
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmatrix <- function(matrix) m <<- matrix
+  getmatrix <- function() m
+  list(set = set, get = get,
+       setmatrix = setmatrix,
+       getmatrix = getmatrix)
 }
 
 
-## Write a short comment describing this function
+## finds the inverse of a matrix, caching the results of the computation through the use 
+## of the proxy defined in makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getmatrix()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setmatrix(m)
+  m
 }
